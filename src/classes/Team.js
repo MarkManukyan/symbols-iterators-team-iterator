6ecd46c130236b59048eb4792f12ccbc5e3da7b5
MarkManukyan <manukanmark90@gmail.com>
2026-04-07 19:24:20 +0300

    Initial commit: Team iterator implementation with Webpack setup

---
+++ src/classes/Team.js
@@ -0,0 +1,21 @@
+class Team {
+  constructor() {
+    this.members = [];
+  }
+
+  add(character) {
+    this.members.push(character);
+  }
+
+  addAll(...characters) {
+    this.members.push(...characters);
+  }
+
+  * [Symbol.iterator]() {
+    for (const member of this.members) {
+      yield member;
+    }
+  }
+}
+
+export default Team;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Team from './classes/Team';
+
+const char1 = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10,
+};
+
+const char2 = {
+  name: 'Маг',
+  type: 'Mage',
+  health: 40,
+  level: 1,
+  attack: 60,
+  defence: 5,
+};
+
+const team = new Team();
+team.addAll(char1, char2);
+
+for (const character of team) {
+  console.log(character);
+}
